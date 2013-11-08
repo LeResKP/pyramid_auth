08431cb42d98ae1c50725587375be02257754f87
Aurélien Matouillot <a.matouillot@gmail.com>
2013-11-08 21:26:51 +0100

    mako.directories is not always defined

--- pyramid_auth/__init__.py
+++ pyramid_auth/__init__.py
@@ -10,10 +10,14 @@ def includeme(config):
         raise Exception('Policy not supported: %s' % policy)
 
     mod = eval_import('pyramid_auth.%s_auth' % policy)
-    mod.includeme(config)
     config.set_authorization_policy(ACLAuthorizationPolicy())
+    mod.includeme(config)
 
     sqladmin_dir = 'pyramid_auth:templates'
+
+    if 'mako.directories' not in settings:
+        settings['mako.directories'] = []
+
     if type(settings['mako.directories']) is list:
         settings['mako.directories'] += [sqladmin_dir]
     else:
